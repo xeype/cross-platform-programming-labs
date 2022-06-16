6a52d433e8f3acc59a249905b161d910984a3a68
xeype <alexeypedko@gmail.com>
2022-06-16 19:02:51 +0300

    update lab06

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -29,17 +29,20 @@ namespace Lab06
             // Throws exception:
             // Console.WriteLine(secondProduct.IsNameNotDefault(secondProduct));
 
+            Console.WriteLine("------------------");
+
             // DLL Part 
             DoubleLinkedList<Product> products = new DoubleLinkedList<Product>();
             products.Add(firstBoat);
-            
             products.AddFirst(firstCar);
-            
             products.Add(firstProduct);
-            
+
             Console.WriteLine(products[1]);
             
             Console.WriteLine("------------------");
+            
+            products.Remove(firstBoat);
+            Console.WriteLine("is products contain first boat: " + products.Contains(firstBoat));
             
             products.ShowAll();
         }
